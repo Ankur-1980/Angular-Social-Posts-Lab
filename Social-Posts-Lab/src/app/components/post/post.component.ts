6f210ddf80a14d03b9335ea3acc7660c5046b353
Ankur <vasavada.ankur@gmail.com>
2020-04-05 11:33:01 -0400

    post basic html & css

--- Social-Posts-Lab/src/app/components/post/post.component.ts
+++ Social-Posts-Lab/src/app/components/post/post.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/interfaces/post';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
+  votes = 0;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  post: Post[] = [
+    {
+      postTitle: 'Grand Circus',
+      postContent: 'Grand Circus is cool',
+      votes: this.votes,
+    },
+    {
+      postTitle: 'Ritual',
+      postContent: 'Ritual is a well developed app',
+      votes: this.votes,
+    },
+    {
+      postTitle: 'Scott',
+      postContent: 'Scott rides scooters',
+      votes: this.votes,
+    },
+  ];
 }
