35bcf84953adc415531a94c5800cb8957341a171
Ankur <vasavada.ankur@gmail.com>
2020-04-05 21:55:11 -0400

    kinda working

--- Social-Posts-Lab/src/app/app.module.ts
+++ Social-Posts-Lab/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppComponent } from './app.component';
 import { SocialPostsComponent } from './components/social-posts/social-posts.component';
@@ -15,8 +16,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     PostComponent,
     PostFormComponent,
   ],
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+  ],
   providers: [],
+  entryComponents: [PostFormComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Social-Posts-Lab/src/app/components/social-posts/social-posts.component.ts
+++ Social-Posts-Lab/src/app/components/social-posts/social-posts.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from 'src/app/interfaces/post';
 import { PostService } from 'src/app/services/post.service';
+import { MatDialog } from '@angular/material/dialog/';
+import { PostFormComponent } from '../post-form/post-form.component';
 
 @Component({
   selector: 'app-social-posts',
@@ -10,10 +12,18 @@ import { PostService } from 'src/app/services/post.service';
 export class SocialPostsComponent implements OnInit {
   posts: Post[];
   votes = 0;
-  constructor(private postService: PostService) {}
+  constructor(public dialog: MatDialog, private postService: PostService) {}
 
   ngOnInit(): void {
     this.posts = this.postService.getPosts();
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(PostFormComponent, {});
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
   }
 
   deleted(post: Post) {
